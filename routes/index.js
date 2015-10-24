f11ae9a422d6b8f14e8d2d391835a04d14b1c520
rodolfoarv <rodolfo@ramirezvalenzuela.com>
2015-10-24 18:00:25 -0500

    Addded index page, form page and the respective methods in the index.js file for the routers, also added the ejs-layouts package

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Hangman!' });
+});
+
+router.get('/form', function(req, res, next) {
+  res.render('form', { title: 'Hangman!' });
+});
+
+router.post('/info', function(req,res){
+  console.log(req.body);
+  res.redirect('/play');
+});
+
+router.get('/play', function(req,res){
+  res.render('play', {title: 'Hangman!'});
 });
 
 module.exports = router;
