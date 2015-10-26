27508087d75f7af63c38573840dcca3d755c59e5
rodolfoarv <rodolfo@ramirezvalenzuela.com>
2015-10-26 16:55:54 -0600

    Fixed some issues with the hangman algorithm, added a new game button inside the game, added glyphicons

--- public/js/play.js
+++ public/js/play.js
@@ -3,6 +3,7 @@ var listOfWords = [];
 var matchResult = false;
 var numberOfTriesBeforeLosing;
 var numberOfLives = 0;
+var lettersMissing;
 
 function getLetter(id){
   return document.getElementById(id).innerHTML.toLowerCase().trim();
@@ -29,65 +30,91 @@ function getLettersPosition(word,letter,arr){
 
 function checkLetter(id,arr,lives){
   document.getElementById(id).className = "btn btn-danger btn-lg disabled"
-
-
+  var letter = getLetter(id);
   if (listOfWords.length == 0){
     listOfWords = arr.split(',');
     if (Math.floor((Math.random() * 10) + 1) >= 8) matchResult = true;
     numberOfLives = lives;
+    lettersMissing = listOfWords[0].length;
     console.log(matchResult);
   }
   var randomIndex = Math.floor(Math.random() * listOfWords.length) + 1;
   var randomWord = listOfWords[randomIndex];
-  if (numberOfLives === 1 && matchResult === false){ //this means the hangman will win
+  if (lettersMissing === 1 && matchResult === false){ //this means the hangman will win
     randomWord = listOfWords[randomWord+1];
-    numberOfLives = 0;
+    numberOfLives --
+    discardLetter(letter);
+    console.log(listOfWords);
     document.getElementById('livesLeft').innerHTML = numberOfLives;
-  }
+  }else{
 
-    var letter = getLetter(id);
-    var auxiliaryListOfWords = [];
-    var inserted = false;
-    //Process to split into classes depending the number of ocurrences
-    for (var i = 0; i < listOfWords.length; i++) {
-      if (listOfWords[i].includes(letter)){
-        for (var j = 0; j < auxiliaryListOfWords.length+1; j++) {
-          if (auxiliaryListOfWords[j] === undefined){
-            if (inserted == false){
-              auxiliaryListOfWords[j] = [];
-              auxiliaryListOfWords[j].push(listOfWords[i]);
-              inserted = true;
+        var auxiliaryListOfWords = [];
+        var inserted = false;
+        //Process to split into classes depending the number of ocurrences
+        for (var i = 0; i < listOfWords.length; i++) {
+          if (listOfWords[i].includes(letter)){
+            for (var j = 0; j < auxiliaryListOfWords.length+1; j++) {
+              if (auxiliaryListOfWords[j] === undefined){
+                if (inserted == false){
+                  auxiliaryListOfWords[j] = [];
+                  auxiliaryListOfWords[j].push(listOfWords[i]);
+                  inserted = true;
+                }
+              }else{
+                if (compareLetters(auxiliaryListOfWords[j][0],listOfWords[i],letter)){
+                  auxiliaryListOfWords[j].push(listOfWords[i]);
+                  inserted=true;
+                }
+              }
             }
-          }else{
-            if (compareLetters(auxiliaryListOfWords[j][0],listOfWords[i],letter)){
-              auxiliaryListOfWords[j].push(listOfWords[i]);
-              inserted=true;
+            inserted = false;
+
+          }
+        }
+        //Sort to get the list with the most amount of words
+
+        auxiliaryListOfWords.sort(function(a,b){
+          return b.length - a.length;
+        });
+        //Migrate the words to the original array and clear the auxiliary unless the word doesn't exist
+        if (auxiliaryListOfWords[0] === undefined){
+          numberOfLives -= 1;
+          document.getElementById('livesLeft').innerHTML = numberOfLives;
+        }else if(auxiliaryListOfWords[0].length < lettersMissing && matchResult === false){
+          discardLetter(letter);
+          numberOfLives -= 1;
+          document.getElementById('livesLeft').innerHTML = numberOfLives;
+        }else{
+          listOfWords = auxiliaryListOfWords[0].slice(0);
+          auxiliaryListOfWords.length = 0;
+          console.log(listOfWords);
+          //Insert the letter into the field
+          var letterPositions = [];
+          getLettersPosition(listOfWords[0],letter,letterPositions);
+          for (var i = 0; i < letterPositions.length; i++) {
+            var id = 'secret' + letterPositions[i];
+            lettersMissing--;
+            document.getElementById(id).innerHTML = letter;
+            if (lettersMissing == 0){
+              document.getElementById('livesLeft').innerHTML = 'You have won!!!';
+              console.log('you have won!');
             }
           }
         }
-        inserted = false;
+  }
 
-      }
-    }
-    //Sort to get the list with the most amount of words
+}
 
-    auxiliaryListOfWords.sort(function(a,b){
-      return b.length - a.length;
-    });
-    //Migrate the words to the original array and clear the auxiliary unless the word doesn't exist
-    if (auxiliaryListOfWords[0] === undefined){
-      numberOfLives -= 1;
-      document.getElementById('livesLeft').innerHTML = numberOfLives;
-    }else{
-      listOfWords = auxiliaryListOfWords[0].slice(0);
-      auxiliaryListOfWords.length = 0;
-      console.log(listOfWords);
-      //Insert the letter into the field
-      var letterPositions = [];
-      getLettersPosition(listOfWords[0],letter,letterPositions);
-      for (var i = 0; i < letterPositions.length; i++) {
-        var id = 'secret' + letterPositions[i];
-        document.getElementById(id).innerHTML = letter;
-      }
+function discardLetter(letter){
+  var auxiliarList = [];
+  for (var i = 0; i < listOfWords.length; i++) {
+    var word = listOfWords[i].trim();
+    if (word.indexOf(letter) < 0){
+      auxiliarList.push(word);
     }
+  }
+  listOfWords = auxiliarList.slice(0);
+  auxiliarList.length = 0;
+
+
 }
