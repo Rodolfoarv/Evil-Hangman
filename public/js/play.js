9a818490fe3b57d5369b40a856a9ae7a8ff15f0c
rodolfoarv <rodolfo@ramirezvalenzuela.com>
2015-10-26 10:20:30 -0600

    Implemented the algorithm for evilHangman

--- public/js/play.js
+++ public/js/play.js
@@ -1,8 +1,66 @@
 var listOfWords = [];
-
+var matchResult = false;
+var numberOfTriesBeforeLosing;
 
 function getLetter(id){
-  return document.getElementById(id).innerHTML;
+  return document.getElementById(id).innerHTML.toLowerCase().trim();
+}
+
+function compareLetters(word1,word2,letter){
+  for (var i = 0; i < word1.length; i++) {
+    if (word1[i] === letter){
+      if(word1[i] !== word2[i]) return false;
+    }
+  }
+  return true;
+}
+
+function checkLetter(id,arr){
+  document.getElementById(id).className = "btn btn-danger btn-lg"
+  if (listOfWords.length == 0){
+    listOfWords = arr.split(',');
+    if (Math.floor((Math.random() * 10) + 1) >= 8) matchResult = true;
+  }
+    var letter = getLetter(id);
+    var auxiliaryListOfWords = [];
+    var inserted = false;
+    //Process to split into classes depending the number of ocurrences
+    for (var i = 0; i < listOfWords.length; i++) {
+      if (listOfWords[i].includes(letter)){
+        for (var j = 0; j < auxiliaryListOfWords.length+1; j++) {
+          if (auxiliaryListOfWords[j] === undefined){
+            if (inserted == false){
+              auxiliaryListOfWords[j] = [];
+              auxiliaryListOfWords[j].push(listOfWords[i]);
+              inserted = true;
+            }
+          }else{
+            if (compareLetters(auxiliaryListOfWords[j][0],listOfWords[i],letter)){
+              auxiliaryListOfWords[j].push(listOfWords[i]);
+              inserted=true;
+            }
+          }
+        }
+        inserted = false;
+
+      }
+    }
+    //Sort to get the list with the most amount of words
+
+    auxiliaryListOfWords.sort(function(a,b){
+      return b.length - a.length;
+    });
+    //Migrate the words to the original array and clear the auxiliary
+    listOfWords = auxiliaryListOfWords[0].slice(0);
+    auxiliaryListOfWords.length = 0;
+    console.log(listOfWords);
+    //Insert the letter into the field
+    for (var i = 0; i < listOfWords[0].length; i++) {
+      listOfWords[0][i]
+    }
+
+
+
 }
 
 
--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.post('/info', function(req,res){
   req.session.name = req.body.name;
   req.session.wordLength = req.body.wordLength;
   req.session.numberOfLives = req.body.numberOfLives;
+  req.session.wordsArray = [];
   console.log(req.session);
   res.redirect('/play');
 });
@@ -25,7 +26,6 @@ router.get('/play', function(req,res){
    var file = path.join(__dirname, 'dictionary.txt');
 
    fs.readFile(file,function(err,words){
-
      if (err) throw err;
      words = words.toString().split('\n');
      var wordsArray = [];
@@ -33,18 +33,18 @@ router.get('/play', function(req,res){
      var wordLength = req.session.wordLength;
      var numberOfLives = req.session.nombre;
      for(var i = 0; i < words.length; i++){
-       if (words[i].length == wordLength){
-         wordsArray.push(words[i]);
-         console.log("I got in here!");
+       var word = words[i].trim();
+       if (word.length == wordLength){
+         wordsArray.push(word);
        }
      }
-     var randomWord = Math.floor((Math.random() * wordsArray.length) +1);
-     console.log(wordsArray[randomWord]);
+     
      var lettersArray=['A','B','C','D','E','F','G','H','I','J','K','L','M',
                   'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
      res.render('play', {title: 'Hangman!',
                          letters: lettersArray,
-                         session: req.session});
+                         session: req.session,
+                         wordsArr: wordsArray});
    });
 
 });
